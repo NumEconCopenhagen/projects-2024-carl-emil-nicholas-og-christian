5132137007dd6dbd21e7a179fd321ec8c75f65bd
fzw199 <fzw199@alumni.ku.dk>
2024-06-12 10:57:02 +0200

    1.4

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -15,16 +15,24 @@ class ExchangeEconomyClass:
         par.w2A = 0.3
 
     def utility_A(self,x1A,x2A):
-        pass
+        par = self.par
+        return x1A**par.alpha * x2A**(1-par.alpha)
 
     def utility_B(self,x1B,x2B):
-        pass
+        par = self.par
+        return x1B**par.beta * x2B**(1-par.beta)
 
     def demand_A(self,p1):
-        pass
+        par = self.par
+        x1A = par.alpha * ((p1*par.w1A + par.w2A)/p1)
+        x2A = (1-par.alpha) * (p1*par.w1A + par.w2A)
+        return x1A,x2A
 
     def demand_B(self,p1):
-        pass
+        par = self.par
+        x1B = par.beta * ((p1*par.w1B + par.w2B)/p1)
+        x2B = (1-par.beta) * (p1*par.w1B + par.w2B)
+        return x1B,x2B
 
     def check_market_clearing(self,p1):
 
