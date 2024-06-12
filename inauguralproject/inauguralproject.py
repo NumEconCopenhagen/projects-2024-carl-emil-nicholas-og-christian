583673a6e75ee300f802b7a757b0f787d05f465d
fzw199 <fzw199@alumni.ku.dk>
2024-06-12 11:12:54 +0200

    1.5

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,4 +1,6 @@
 from types import SimpleNamespace
+import numpy as np
+import matplotlib.pyplot as plt
 
 class ExchangeEconomyClass:
 
@@ -13,6 +15,8 @@ class ExchangeEconomyClass:
         # b. endowments
         par.w1A = 0.8
         par.w2A = 0.3
+        par.w1B = 1 - par.w1A
+        par.w2B = 1 - par.w2A
 
     def utility_A(self,x1A,x2A):
         par = self.par
@@ -33,15 +37,33 @@ class ExchangeEconomyClass:
         x1B = par.beta * ((p1*par.w1B + par.w2B)/p1)
         x2B = (1-par.beta) * (p1*par.w1B + par.w2B)
         return x1B,x2B
-
-    def check_market_clearing(self,p1):
-
+    
+    def plot_edgeworth_box(self):
         par = self.par
-
-        x1A,x2A = self.demand_A(p1)
-        x1B,x2B = self.demand_B(p1)
-
-        eps1 = x1A-par.w1A + x1B-(1-par.w1A)
-        eps2 = x2A-par.w2A + x2B-(1-par.w2A)
-
-        return eps1,eps2
+        N = 75
+        x1A_grid = np.linspace(0, 1, N)
+        x2A_grid = np.linspace(0, 1, N)
+        
+        X1A, X2A = np.meshgrid(x1A_grid, x2A_grid)
+        UA = self.utility_A(X1A, X2A)
+        UB = self.utility_B(1-X1A, 1-X2A)
+        
+        UA_endowment = self.utility_A(par.w1A, par.w2A)
+        UB_endowment = self.utility_B(par.w1B, par.w2B)
+        
+        plt.figure(figsize=(10, 10))
+        
+        plt.contour(X1A, X2A, UA, levels=[UA_endowment], colors='blue', linestyles='dotted')
+        plt.contour(X1A, X2A, UB, levels=[UB_endowment], colors='red', linestyles='dotted')
+        
+        plt.fill_between(X1A.flatten(), X2A.flatten(), where=(UA >= UA_endowment) & (UB >= UB_endowment), color='green', alpha=0.3)
+        
+        plt.plot(par.w1A, par.w2A, 'bo', label='Endowment A')
+        plt.plot(par.w1B, par.w2B, 'ro', label='Endowment B')
+        
+        plt.xlabel('Good 1')
+        plt.ylabel('Good 2')
+        plt.title('Edgeworth Box')
+        plt.legend()
+        plt.grid()
+        plt.show()
